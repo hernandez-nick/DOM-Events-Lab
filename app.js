b41ce5c089591c7ed642b6d847a51295bfebf8e3
hernandeznick-ga <hernandeznick1990@gmail.com>
2025-12-14 18:53:54 -0800

    third commit

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 /*-------------------------------- Constants --------------------------------*/
 const calculator = document.querySelector('#calculator');
-
 const display = document.querySelector('.display');
 /*-------------------------------- Variables --------------------------------*/
 let firstInput = '';
-
 let secondInput = '';
-
 let operator = '';
-
 let displayValue = '0';
 /*------------------------ Cached Element References ------------------------*/
 display.innerText = 0;
 /*----------------------------- Event Listeners -----------------------------*/
 calculator.addEventListener('click', (event) => {
     console.log(event.target.innerText);
-
   
   if (event.target.classList.contains('number')) {
     firstInput += event.target.innerText;
@@ -35,27 +30,21 @@ calculator.addEventListener('click', (event) => {
       secondInput = '';
       operator = '';
     }
-  }
-  if (event.target.innerText === '=') {
-    if (operator === '/') {
+      else if (operator === '/') {
       displayValue = Number(secondInput) / Number(firstInput);
       display.innerText = displayValue;
       firstInput = displayValue;
       secondInput = '';
       operator = '';
     }
-  }
-  if (event.target.innerText === '=') {
-    if (operator === '+') {
+      else if (operator === '+') {
       displayValue = Number(secondInput) + Number(firstInput);
       display.innerText = displayValue;
       firstInput = displayValue;
       secondInput = '';
       operator = '';
     }
-  }
-  if (event.target.innerText === '=') {
-    if (operator === '-') {
+      else if (operator === '-') {
       displayValue = Number(secondInput) - Number(firstInput);
       display.innerText = displayValue;
       firstInput = displayValue;
@@ -71,5 +60,15 @@ calculator.addEventListener('click', (event) => {
     display.innerText = displayValue;
   }
 });
-
 /*-------------------------------- Functions --------------------------------*/
+function calculate() {
+  if (operator === '*') {
+    return Number(secondInput) * Number(firstInput);
+  } else if (operator === '/') {
+    return Number(secondInput) / Number(firstInput);
+  } else if (operator === '+') {
+    return Number(secondInput) + Number(firstInput);
+  } else if (operator === '-') {
+    return Number(secondInput) - Number(firstInput);
+  }
+}
